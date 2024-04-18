6908c82db3f80411648efe862f1c18b4445ff084
Mateusz Świątek <mswiatek2002@gmail.com>
2024-04-19 01:17:20 +0200

    It's alive! Generating a simple implementation, creating an empty implementation.

---
+++ MyInterfaceImpl.java
@@ -0,0 +1,17 @@
+public class MyInterfaceTestImpl implements MyInterfaceTest {
+    @Override
+    public void method1() {
+        // Tutaj bedzie implementacja moze kiedys method1
+    }
+
+    @Override
+    public Integer method3() {
+        // Tutaj bedzie implementacja moze kiedys method3
+    }
+
+    @Override
+    public String method2() {
+        // Tutaj bedzie implementacja moze kiedys method2
+    }
+
+}
